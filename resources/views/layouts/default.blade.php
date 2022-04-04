c83b951c1c46e1b9c1a0262fb635a3a05c740c41
zhangqg <zhangqgxh@gmail.com>
2022-04-04 03:31:42 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
