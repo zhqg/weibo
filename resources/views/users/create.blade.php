46e1dc3e820ca5c38fff5862d21ff5e745cb4ea9
zhangqg <zhangqgxh@gmail.com>
2022-04-18 14:52:04 +0000

    完成用户注册功能

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -31,7 +31,7 @@
 
         <div class="mb-3">
           <label for="password_confirmation">确认密码：</label>
-          <input type="password" name="password_comfirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
         </div>
 
         <button type="submit" class="btn btn-primary">注册</button>
